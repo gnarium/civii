ec66b54002a014352445d505243916e9914d7b7f
gnarium <80353633+gnarium@users.noreply.github.com>
2021-03-16 02:03:00 +0530

    web file update

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,31 @@ Route::get('/', function () {
 });
 Route::view('/','front_end/index');
 Route::view('/about','front_end/about');
-Route::view('/contact','front_end/contact');
 Route::view('/privacy','front_end/privacy');
 Route::view('/disclaimer','front_end/disclaimer');
 
+
+
+
+Route::get('/front_end/contact',[App\Http\Controllers\AdminController::class,'contact'])->name('admin');
+Route::post('/front_end/contact',[App\Http\Controllers\AdminController::class,'contact_db'])->name('admin');
 
 
 Route::get('/admin/adminindex',[App\Http\Controllers\AdminController::class,'adminindex'])->name('admin');
 
 Route::get('/admin/adminpages/privatejob',[App\Http\Controllers\AdminController::class,'privatejob'])->name('admin');
 Route::post('/admin/adminpages/privatejob',[App\Http\Controllers\AdminController::class,'privatejob_db'])->name('admin');
-Route::get('front_end/privatejob',[App\Http\Controllers\AdminController::class,'privatejobdetails'])->name('admin');
+Route::get('/front_end/privatejob',[App\Http\Controllers\AdminController::class,'privatejobdetails'])->name('admin');
+Route::get('/front_end/privatejob/{tab}',[App\Http\Controllers\AdminController::class,'privatejobdetailsdescribtion'])->name('admin');
 
+Route::match(['get','post'],'/admin/login',[App\Http\Controllers\AdminController::class,'login'])->name('admin');
 
 Route::get('/admin/adminpages/govtjob',[App\Http\Controllers\AdminController::class,'govtjob'])->name('admin');
 Route::post('/admin/adminpages/govtjob',[App\Http\Controllers\AdminController::class,'govtjob_db'])->name('admin');
 Route::get('/front_end/govtjob',[App\Http\Controllers\AdminController::class,'govtjobdetails'])->name('admin');
+
+Route::get('/front_end/govtjob/{tab}',[App\Http\Controllers\AdminController::class,'jobdetailsdescribtion'])->name('admin');
+
 
 Route::get('/admin/adminpages/qa',[App\Http\Controllers\AdminController::class,'qa'])->name('admin');
 Route::post('/admin/adminpages/qa',[App\Http\Controllers\AdminController::class,'qa_db'])->name('admin');
