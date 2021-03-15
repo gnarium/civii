1653a85de4f3a931b0258b6652dfe40c96489c17
gnarium <80353633+gnarium@users.noreply.github.com>
2021-03-16 02:10:47 +0530

    jobdesc code update

--- resources/views/front_end/jobdescribtion.blade.php
+++ resources/views/front_end/jobdescribtion.blade.php
@@ -97,7 +97,7 @@
 							<li> <a href="/disclaimer">Disclaimer</a>
 								
 							</li>
-							<li> <a href="/contact">Contact us </a>
+							<li> <a href="/front_end/contact">Contact us </a>
 								
 							</li>
 						</ul>
@@ -112,7 +112,7 @@
 
     <div class="page-content bg-white">
         <!-- inner page banner -->
-        <div class="dez-bnr-inr overlay-black-middle" style="background-image:url(images/background/bg4.jpg);">
+        <div class="dez-bnr-inr overlay-black-middle" style="background-image:url('{{ asset('user/front_end/images/background/bg4.jpg')}}');">
             <div class="container">
                 <div class="dez-bnr-inr-entry">
                     <h1 class="text-white">Job Details</h1>
@@ -135,95 +135,14 @@
             <!-- Career -->
             <div class="container">
                 <div class="row">
-                    <!-- Left part start -->
-                    <div class="col-lg-9 col-md-7 m-b30">
-                        <h1 class="m-b20 m-t0">Openings </h1>
+				    <!-- Left part start -->
+                    <div class="col-lg-12 col-md-7 m-b30">
+                        <h1 class="m-b20 m-t0">Job Describtion </h1>
                         <div class="dez-separator bg-primary"></div>
-                        <p><strong>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                            standard dummy text ever since the  when an unknown printer took a galley of type and scrambled it to make a 
-                            type specimen book. </strong></p>
-                        <h2 class="m-t30 m-b10 ">Business Development Manager</h2>
-                        <ul class="list-angle-right">
-                            <li><strong>Work Status:</strong> Full time</li>
-                            <li><strong>Qualifications:</strong> BE/B.Tech/MCA/BCA</li>
-                            <li><strong>Experience:</strong> 1-3 Years</li>
-                            <li><strong>Location:</strong> UK </li>
-                        </ul>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-                            text ever since the  when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is 
-                            simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-                            text ever since the  when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                        <h3>Our Offerings</h3>
-                        <ul class="list-check-circle primary">
-                            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</li>
-                            <li>Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</li>
-                            <li>Printer took a galley of type and scrambled it to make a type specimen book. </li>
-                            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</li>
-                            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy book. </li>
-                            <li>Eorem ipsum dolor sit amete printing and typesetting industry. </li>
-                        </ul>
-                        <a href="#" class="site-button "><span>Apply Now</span><i class="fa fa-angle-right m-l10"></i></a>
-                        <h2 class="m-t30 m-b10 ">Construction Development</h2>
-                        <ul class="list-angle-right">
-                            <li><strong>Work Status:</strong> Full time</li>
-                            <li><strong>Qualifications:</strong> BE/B.Tech/MCA/BCA</li>
-                            <li><strong>Experience:</strong> 1-3 Years</li>
-                            <li><strong>Location:</strong> UK </li>
-                        </ul>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
-                            text ever since the  when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply 
-                            dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the  when an 
-                            unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-                            the industry's standard dummy text ever since the  when an unknown printer took a galley of type and 
-                            scrambled it to make a type specimen book. </p>
-                        <h3>Our Offerings</h3>
-                        <ul class="list-check-circle primary">
-                            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</li>
-                            <li>Dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</li>
-                            <li>Printer took a galley of type and scrambled it to make a type specimen book. </li>
-                            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</li>
-                            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy book. </li>
-                            <li>Eorem ipsum dolor sit amete printing and typesetting industry. </li>
-                        </ul>
+                        <p><strong>{{$todoArr14->describtion}}</strong></p>
                         <a href="#" class="site-button "><span>Apply Now</span><i class="fa fa-angle-right m-l10"></i></a> </div>
-                    <!-- Left part start END -->
-                    <!-- Right part start -->
-                    <div class="col-lg-3 col-md-5">
-                        <div class="row">
-							<div class="col-lg-12 col-md-12 col-sm-6">
-								<div class="icon-bx-wraper bx-style-1 p-a30 center m-b15">
-									<div class="icon-bx-sm text-primary bg-white radius border-2 m-b20"> <a href="#" class="icon-cell"><i class="fa fa-user"></i></a> </div>
-									<div class="icon-content">
-										<h5 class="dez-tilte text-uppercase">Recent Highlights</h5>
-										<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod .</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-12 col-md-12 col-sm-6">
-								<div class="icon-bx-wraper bx-style-1 p-a30 center m-b15">
-									<div class="icon-bx-sm text-primary bg-white radius border-2 m-b20"> <a href="#" class="icon-cell"><i class="fa fa-building-o"></i></a> </div>
-									<div class="icon-content">
-										<h5 class="dez-tilte text-uppercase">Recent Highlights</h5>
-										<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod .</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-12 col-md-12 col-sm-12">
-								<div class="icon-bx-wraper bx-style-1 p-a20 left">
-									<div class="icon-bx-xs text-primary bg-white radius border-2 "> <a href="#" class="icon-cell"><i class="fa fa-code"></i></a> </div>
-									<div class="icon-content">
-										<h5 class="dez-tilte text-uppercase">Content title</h5>
-										<p>Lorem ipsum dolor sit elit nonummy dolor is euismod end .</p>
-									</div>
-								</div>
-							</div>
-						</div>
-                    </div>
-                    <!-- Right part start END -->
-                </div>
+                  
+    			</div>
             </div>
             <!-- Career END -->
         </div>
