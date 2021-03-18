e31aec22b52a30ddcde0ea268c685cbba5332816
gnarium <80353633+gnarium@users.noreply.github.com>
2021-03-19 00:56:31 +0530

    footer changed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,41 +2,53 @@
 		<div class="footer-top">
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-4 col-md-6 col-sm-6 footer-col-4">
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
 						<div class="widget widget_about">
 							<div class="logo-footer"><img src="{{ asset('user/front_end/images/logooo.png')}}" alt=""></div>
-							<p class="m-tb20"><strong>Construct</strong>Dear candidate, we are not a recruiter/job consultant. We only post the available and relevant jobs from the internet, we are not involved directly or indirectly in the recruitment process at any stage.</p>
-							<ul class="dez-social-icon dez-social-icon-lg">
-								<li><a href="javascript:void(0);" class="fa fa-facebook fb-btn"></a></li>
-								<li><a href="javascript:void(0);" class="fa fa-twitter tw-btn"></a></li>
-								<li><a href="javascript:void(0);" class="fa fa-linkedin link-btn"></a></li>
-								<li><a href="javascript:void(0);" class="fa fa-pinterest-p pin-btn"></a></li>
-							</ul>
+							<p class="m-tb20">Dear candidate, we are not a recruiter/job consultant. We only post the available and relevant jobs from the internet, we are not involved directly or indirectly in the recruitment process at any stage.</p>
+								
 						</div>
 					</div>
-					<div class="col-lg-4 col-md-6 col-sm-6 footer-col-4">
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
 						<div class="widget widget_services">
 							<h4 class="m-b15 text-uppercase">Useful Links</h4>
 							<div class="dez-separator bg-primary"></div>
 							<ul>
 								<li><a href="/about">About</a></li>
-								<li><a href="front_end/govtjob">Government Job's</a></li>
-								<li><a href="front_end/privatejob">Private Job's</a></li>
+								<li><a href="/govtjob">Government Job's</a></li>
+								<li><a href="/privatejob">Private Job's</a></li>
 								<li><a href="/privacy">Privacy & Policy</a></li>
-								<li><a href="/front_end/contact">Contact</a></li>
+								<li><a href="/contact">Contact</a></li>
+								<li><a href="/iscode">IS CODES</a></li>
+								<li><a href="/qa">QA/QC Reports</a></li>
 							</ul>
 						</div>
 					</div>
-					<div class="col-lg-4 col-md-6 col-sm-6 footer-col-4">
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
 						<div class="widget widget_getintuch">
 							<h4 class="m-b15 text-uppercase">Contact us</h4>
 							<div class="dez-separator bg-primary"></div>
 							<ul>
 								<li><i class="fa fa-map-marker"></i><strong>address</strong> ALAMBAGH LUCKNOW 226005</li>
-								<li><i class="fa fa-phone"></i><strong>phone</strong>+91 8899296385</li>
+								<li><i class="fa fa-envelope"></i><strong>Email</strong>engineeringdestination140@gmail.com</li>
 							</ul>
 						</div>
 					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+						<div class="widget widget_getintuch">
+						<h4 class="m-b15 text-uppercase">Follow Us On</h4>
+							
+							<ul class="dez-social-icon dez-social-icon-lg">
+								<li><a href="https://www.facebook.com/CIVIL-ENGG-Destination-100649208640067" class="fa fa-facebook fb-btn"></a></li>
+								<li><a href="https://t.me/civilenggdestination" class="fa fa-telegram tw-btn"></a></li>
+								<li><a href="https://www.linkedin.com/in/civilengineeringdestination-43255b1b6/" class="fa fa-linkedin link-btn"></a></li>
+								<li><a href="https://www.instagram.com/civilengineeringdestination" class="fa fa-instagram pin-btn"></a></li>
+							
+							</ul>
+							
+						</div>
+					</div>
+					
 				</div>
 			</div>
 		</div>
@@ -44,9 +56,9 @@
 		<div class="footer-bottom">
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-4 text-left"> <span>© Copyright 2020</span> </div>
-					<div class="col-lg-4 text-center"> <span> Design With <i class="fa fa-heart text-primary heart"></i> By Gnarium Iqnovatives Pvt. Ltd. </span> </div>
-					<div class="col-lg-4 text-right "> <a href="/about"> About</a> <a href="#"> Help Desk</a> <a href="/privacy"> Privacy Policy</a> </div>
+					<div class="col-lg-4 text-left"> <span>© Copyright 2021</span> </div>
+					<div class="col-lg-4 text-center"> <span> Designed & Developed <i class="fa fa-heart text-primary heart"></i> By Gnarium Iqnovatives Pvt. Ltd. </span> </div>
+					<div class="col-lg-4 text-right "> All Right Reserved | Engineering Destination </div>
 				</div>
 			</div>
 		</div>
