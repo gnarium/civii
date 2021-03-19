f3bf56c373e4bf7591ef089446dbe00a2187023a
gnarium <80353633+gnarium@users.noreply.github.com>
2021-03-20 00:28:02 +0530

    end

--- resources/views/front_end/privacy.blade.php
+++ resources/views/front_end/privacy.blade.php
@@ -142,29 +142,67 @@
                 <div class="row">
                     <!-- Left part start -->
                     <div class="col-lg-12 col-md-7">
-                        <h1 class="m-b20 m-t0">Welcome to Engineering's Destination Privacy Policy </h1>
-                        <div class="dez-separator bg-primary"></div>
-                        <!-- <ul class="list-check-circle primary">
-                            <li>THE CORE PURPOSE OF THE CHANNEL IS TO GIVE AUTHENTIC JOB UPDARTES And IMPORTANT NOTIFICATIONS. HOWEVER, WE DOES NOT GUARENTEE EMPLOYMENT.</li>
-                            <li>WE DO NOT CHARGE ANY FEES, MONEY OR DONATION FROM ANY CANDIDATE FOR ANY EMPLOYMENT .</li>
-                            <li>WE DO NOT PROVIDE ANY REFERENCE FOR ANY JOBS OR EMPLOYMENT. </li>
+                        <h1 class="m-b20 m-t0">Privacy Policy for Engineering Destination</h1>
+                        <p style="text-align:justify;">At Engineering Destination, accessible from www.engineeringdestination.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Engineering Destination and how we use it.
+If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
+This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Engineering Destination. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+						<div class="dez-separator bg-primary"></div>
+						<h2 class="m-b20 m-t0">Consent</h2>
+						<p style="text-align:justify;">By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+                         <ul class="list-check-circle primary">
+						 Information we collect
+						    <li>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</li>
+                            <li>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</li>
+                            <li> When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</li>
                             <li>THE CHANNEL DOES NOT GUARENTEE ANY CHANGES MADE BY JOB GIVING ORGANIZATION FOR ANY UPDATES, CANCELLATION, OR UPDATES AFTER THE PUBLISHING DATE OF NOTIFICATION.</li>
                             <li>COPYING THE CONTENT OF THIS CHANNEL OR WEBSITE IS STRICTLY PROHIBITED.</li>
                             <li>ANY MISUSE OR MISGUIDENCE SHALL BE IMMEDIATELY REPORTED TO THE OWNER OF THE WEBSITE.</li>
                         </ul>
-                         -->
-                        <h3>Term & Conditions </h3>
-                        <!-- <p>WELCOME TO ENGINEERING DESTINATION THESE TERMS AND CONDITIONS OUTLINE THE RULES AND REGULATIONS FOR THE USE OF ENGINEERING DESTINATION</p> -->
-                        <!-- <ul class="list-check-circle primary">
-                            <li>BY ACCESSING THIS WEBSITE WE ASSUME YOU ACCEPT THESE TERMS AND CONDITIONS. DO NOT CONTINUE TO USE WEBSITE NAME IF YOU DO NOT AGREE TO TAKE ALL OF THE TERMS AND CONDITIONS STATED ON THIS PAGE.</li>
-                            <li>WE DO NOT OFFER EMPLOYMENT.</li>
-                            <li>WE DO NOT HAVE ANY COLLABARATION FROM ANY ORGANIZATION OR CONSULTANCY. </li>
-                            <li>ENGINEERING DESTINATION DO NOT TAKE ANY MONEY FROM ANY INDIVIDUAL OR ANY ORGANIZATION.</li>
-                            <li>WE DO NOT TAKE RESPONSIBITY FOR ANY MONEY ASKED FROM ANY THIRD PARTY.</li>
-
+                        <h3>How we use your information</h3>
+                        <p>We use the information we collect in various ways, including to:</p> 
+                        <ul class="list-check-circle primary">
+                            <li>Provide, operate, and maintain our website.</li>
+                            <li>Improve, personalize, and expand our website.</li>
+                            <li>Understand and analyze how you use our website. </li>
+                            <li>Develop new products, services, features, and functionality.</li>
+                           <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+						   <li>Send you emails</li>
+						   <li>Find and prevent fraud</li>
                         </ul>
+                        <h2 class="m-b20 m-t0">Log Files</h2>
+						<p style="text-align:justify;">Engineering Destination follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+                        <h2 class="m-b20 m-t0">Cookies and Web Beacons</h2>
+						<p style="text-align:justify;">Like any other website, Engineering Destination uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.For more general information on cookies, please read "What Are Cookies" from Cookie Consent.</p>
+						<h2 class="m-b20 m-t0">Google DoubleClick DART Cookie</h2>
+						<p style="text-align:justify;">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – </p>
+						<a href="https://policies.google.com/technologies/ads" target="_blank"></a>
+                        <h2 class="m-b20 m-t0">Advertising Partners Privacy Policies</h2>
+						<p style="text-align:justify;">You may consult this list to find the Privacy Policy for each of the advertising partners of Engineering Destination.Third-party ad servers or ad networks use technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Engineering Destination, which are sent directly to users' browsers. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.Note that Engineering Destination has no access to or control over these cookies that are used by third-party advertisers.</p>
+						<h2 class="m-b20 m-t0">Third Party Privacy Policies</h2>
+						<p style="text-align:justify;">Engineering Destination's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.</p>
+						<p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
+						<h2 class="m-b20 m-t0">CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
+						<p style="text-align:justify;">Under the CCPA, among other rights, California consumers have the right to:
+Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+Request that a business delete any personal data about the consumer that a business has collected.
+Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
+If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+						<h2 class="m-b20 m-t0">GDPR Data Protection Rights</h2>
+						<p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+						<ul class="list-check-circle primary">
+                            <li>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</li>
+                            <li>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</li>
+                            <li>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</li>
+                            <li>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</li>
+                           <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+						   <li>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</li>
+						   <li> The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
+                           <li> If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</li>
+						<h2 class="m-b20 m-t0">Children's Information</h2>
+						<p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.Engineering Destination does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+						</ul>
                         
-						</div> -->
+						</div> 
                     <!-- Left part start END -->
                     <!-- Right part start -->
                                         <!-- Right part start END -->
