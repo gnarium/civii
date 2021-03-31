6cf954070b1c26887e3d94362c7fbb3b72759e79
gnarium <80353633+gnarium@users.noreply.github.com>
2021-03-31 17:55:07 +0530

    end

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,12 +20,12 @@ class AdminController extends Controller
     public function govtjobdetail()
     {
         $todoArr13 = DB::table('govtjobs')
-        ->whereBetween('id',[5,8])
+        ->whereBetween('id',[0,3])
         ->get();
-//          $todoArr110=DB::table('privatejobs')
-//         ->whereBetween('id',[100001,200001])
-//         ->get();
-        $todoArr110 = DB::select("select * from privatejobs");
+         $todoArr110=DB::table('privatejobs')
+         ->whereBetween('id',[0,3])
+         ->get();
+//         $todoArr110 = DB::select("select * from privatejobs");
        
         return view('front_end/index',['todoArr13'=>$todoArr13,'todoArr110'=>$todoArr110]);
     }
