ee8fc1415e1c6b7731b0caf4575187635b661bb6
gnarium <80353633+gnarium@users.noreply.github.com>
2021-03-31 11:50:29 +0530

    field add

--- resources/views/admin/adminpages/govtjob.blade.php
+++ resources/views/admin/adminpages/govtjob.blade.php
@@ -95,6 +95,12 @@
                         <div class=" form">
                             <form class="cmxform form-horizontal" method="post" action="/admin/adminpages/govtjob" novalidate="novalidate"  enctype="multipart/form-data">
                                 {{ csrf_field()}}
+                                <div class="form-group ">
+                                    <label for="cname" class="control-label col-lg-3">Job Title (required)</label>
+                                        <div class="col-lg-6">
+                                            <input class=" form-control" id="cname" name="Jobtitle" type="text" required="">
+                                        </div>
+                                    </div>
                                     <div class="form-group ">
                                         <label for="ccomment" class="control-label col-lg-3">Enter Company Name (required)</label>
                                         <div class="col-lg-6">
