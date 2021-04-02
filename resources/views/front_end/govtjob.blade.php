17b02b77fc5c103960964e4a3ad26ff789d02b85
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-02 13:06:00 +0530

    end

--- resources/views/front_end/govtjob.blade.php
+++ resources/views/front_end/govtjob.blade.php
@@ -147,7 +147,7 @@
 						</div>
 						<div class="row" id="masonry">
                         @foreach($todoArr12 as $todo1)
-							<div class="col-md-6 col-lg-4 col-sm-12 m-b30 product-item card-container">
+							<div class="col-md-6 col-lg-4 col-sm-6 m-b30 product-item card-container">
 								<div class="dez-box "style="background-color:#FDC716">
 									<div class="dez-info p-a20">
 										<h5 class="m-b0" style="text-align:left;"><span>{{$todo1->jobtitle}}</span></h5>
