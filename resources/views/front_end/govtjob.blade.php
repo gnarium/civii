cd7d0b281fb3bf3616896c8d873d0d538eb96e31
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-02 19:20:24 +0530

    end

--- resources/views/front_end/govtjob.blade.php
+++ resources/views/front_end/govtjob.blade.php
@@ -149,7 +149,7 @@
 							<div class="col-md-6 col-lg-6 col-sm-12 m-b30 product-item card-container">
 								<div class="dez-box "style="background-color:#FDC716">
 									<div class="dez-info p-a20" style="margin-left:-10px;">
-										<p class="m-b0"><span>{{$todo1->jobtitle}}</span></p>
+										<p class="m-b0"><b><span>{{$todo1->jobtitle}}</span></b></p>
 										<br/>
                                         <p class="m-b0"><b>Company Name:</b><span>{{$todo1->companyname}}</span></p>
                                        <p class="m-b0"><b>Designation:</b><span>{{$todo1->jobname}}</span></p>
