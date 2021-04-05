ae8e48f0c2edb4c995ddad93894ebd7fa3556762
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-06 01:45:26 +0530

    end

--- resources/views/front_end/iscodedisplay.blade.php
+++ resources/views/front_end/iscodedisplay.blade.php
@@ -9,23 +9,18 @@
 	<meta property="og:description" content="" />
 	<meta property="og:image" content="" />
 	<meta name="format-detection" content="telephone=no">
-	<meta name="google-site-verification" content="ucTRTXLEo1IKrfkR8YoZtee9XpyNdwghNzkuH1hpZcU" />
-
-	<script src="{{ asset('user/front_end/js/combining.js')}}"></script>
-	<link rel="canonical" href="https://www.engineeringdestination.com"/>
+	<link rel="canonical" href="https://engineeringdestination.com"/>
+	<link rel="canonical" href="http://www.engineeringdestination.com"/>
 	<link rel="icon" href="{{ asset('user/front_end/images/fav.ico')}}" type="image/x-icon" />
-	
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
 	
 	<title>Engineering Destination</title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- STYLESHEETS -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/plugins.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/plugins.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/fontawesome/css/font-awesome.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/themify/themify-icons.css')}}">
-	
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/style.min.css')}}">
 	<link class="skin" rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/skin/skin-1.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/templete.min.css')}}">
@@ -36,26 +31,9 @@
 	</style>
 	
 	<!-- Revolution Slider Css -->
-    <link rel="stychildren()lesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/layers.css')}}">
-	
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/layers.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/settings.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/navigation.css')}}">
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<script> 
-		
-		$(document).load(function() {
-            $("body").children().css({
-                "display": "none"
-    		});
-			$("footer").children().css({
-				"display": "none"
-			});
-		});
-		
-	</script>
-<div id="loading-area"></div>
 
 <header class="site-header header-style-4 dark">
 		<!-- top bar -->
