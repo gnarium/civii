5b8c7f94c6a56b4aafd8165ed3d3ff43b0ba7bb7
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-06 01:25:33 +0530

    end

--- resources/views/front_end/about.blade.php
+++ resources/views/front_end/about.blade.php
@@ -1,4 +1,61 @@
 @extends('layouts.main')
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="description" content="" />
+	<meta property="og:title" content="Engineering Destination"/>
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
+	<meta name="format-detection" content="telephone=no">
+	<meta name="google-site-verification" content="ucTRTXLEo1IKrfkR8YoZtee9XpyNdwghNzkuH1hpZcU" />
+
+	<script src="{{ asset('user/front_end/js/combining.js')}}"></script>
+	<link rel="canonical" href="https://www.engineeringdestination.com"/>
+	<link rel="icon" href="{{ asset('user/front_end/images/fav.ico')}}" type="image/x-icon" />
+	
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+	
+	<title>Engineering Destination</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- STYLESHEETS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/plugins.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/fontawesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/themify/themify-icons.css')}}">
+	
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/style.min.css')}}">
+	<link class="skin" rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/skin/skin-1.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/templete.min.css')}}">
+	
+	<!-- Google Font -->
+	<style>
+	@import url('https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900|Open+Sans:300,400,600,700,800|Roboto:100,300,400,500,700,900');
+	</style>
+	
+	<!-- Revolution Slider Css -->
+    <link rel="stychildren()lesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/layers.css')}}">
+	
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/settings.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/revolution/revolution/css/navigation.css')}}">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<script> 
+		
+		$(document).load(function() {
+            $("body").children().css({
+                "display": "none"
+    		});
+			$("footer").children().css({
+				"display": "none"
+			});
+		});
+		
+	</script>
+<div id="loading-area"></div>
 <header class="site-header header-style-4 dark">
 		<!-- top bar -->
 		<div class="top-bar">
