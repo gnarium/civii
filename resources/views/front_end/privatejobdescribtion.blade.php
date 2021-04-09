6afc9d06fcb7c314bd0864bb009cee2774978c6c
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-09 21:15:40 +0530

    end

--- resources/views/front_end/privatejobdescribtion.blade.php
+++ resources/views/front_end/privatejobdescribtion.blade.php
@@ -10,7 +10,13 @@
 	<meta property="og:image" content="" />
 	<meta name="format-detection" content="telephone=no">
 	<meta name="google-site-verification" content="ucTRTXLEo1IKrfkR8YoZtee9XpyNdwghNzkuH1hpZcU" />
-
+<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+  		gtag('config', 'G-R6MCW7DRNV');
+	</script>
+	
 	<script src="{{ asset('user/front_end/js/combining.js')}}"></script>
 	<link rel="canonical" href="https://www.engineeringdestination.com"/>
 	<link rel="icon" href="{{ asset('user/front_end/images/fav.ico')}}" type="image/x-icon" />
