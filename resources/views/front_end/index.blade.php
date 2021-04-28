5106d120cf491b495a2a0c2c16ceb2b22b9a74c9
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-28 22:40:05 +0530

    adds

--- resources/views/front_end/index.blade.php
+++ resources/views/front_end/index.blade.php
@@ -196,7 +196,19 @@
 		<!-- main header END -->
 	</header>
 
-	
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="1194634110"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
 <div id="demo" class="carousel slide" data-ride="carousel">
 
 <!-- Indicators -->
@@ -595,3 +607,15 @@
                 </div>
 		   </div>
          </div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="1194634110"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
