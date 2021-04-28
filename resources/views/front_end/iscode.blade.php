178bce11b4bc6db370b2b899f571934e70b633ff
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-28 22:20:27 +0530

    delete

--- resources/views/front_end/iscode.blade.php
+++ resources/views/front_end/iscode.blade.php
@@ -247,18 +247,7 @@
             <tbody>
                 <tr>
                     <td data-breakpoints="xs">{{$todo->id}}</td>
-                    <td><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
-     crossorigin="anonymous"></script>
-<!-- engineering destination display ads -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="6283309266"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script><a href="{{$todo->url}}">{{$todo->iscode}}</a></td>
+                    <td><a href="{{$todo->url}}">{{$todo->iscode}}</a></td>
                     <td>{{$todo->isdescribtion}}</td>
                 </tr>
        </tbody>
