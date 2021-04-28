9e32125e4a09e74b62792799d24201f7302d3c4c
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-28 22:28:58 +0530

    add

--- resources/views/front_end/privatejob.blade.php
+++ resources/views/front_end/privatejob.blade.php
@@ -245,4 +245,14 @@
 		   </div>
         </div>
     </div>
-  
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="1194634110"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
