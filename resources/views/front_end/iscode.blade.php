d71f0bd92e089c19fc27b4dc527e84c5ab8ef499
gnarium <80353633+gnarium@users.noreply.github.com>
2021-04-28 22:26:14 +0530

    end

--- resources/views/front_end/iscode.blade.php
+++ resources/views/front_end/iscode.blade.php
@@ -196,7 +196,19 @@
                 </ul>
             </div>
         </div>
-     
+     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
+     crossorigin="anonymous"></script>
+<!-- engineering destination display ads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="6283309266"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+        
 		<div class="content-area">
         <div class="container">
                 <div class="row">
