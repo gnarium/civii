419d20e56b500828626fb2a3a4339d7e273ad7a6
gnarium <80353633+gnarium@users.noreply.github.com>
2021-05-25 19:14:38 +0530

    tag manager add

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,7 +24,12 @@
 	<title>Engineering Destination</title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKNSJHM');</script>
+
 	<!-- STYLESHEETS -->
     <link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/css/plugins.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('user/front_end/plugins/fontawesome/css/font-awesome.min.css')}}">
