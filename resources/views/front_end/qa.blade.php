9bdc10ac793a9350a9858c2eb8965e9fb369f0b2
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-03 17:04:44 +0530

    add tah manager

--- resources/views/front_end/qa.blade.php
+++ resources/views/front_end/qa.blade.php
@@ -12,6 +12,13 @@
 	<meta name="google-site-verification" content="ucTRTXLEo1IKrfkR8YoZtee9XpyNdwghNzkuH1hpZcU" />
 	<script src="{{ asset('user/front_end/js/combining.js')}}"></script>
 	<link rel="canonical" href="https://www.engineeringdestination.com"/>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKNSJHM');</script>
+<!-- End Google Tag Manager -->
 	<link rel="icon" href="{{ asset('user/front_end/images/Engineering Destination Favicon .webp')}}" type="image/x-icon" />
 	
 	<title>Engineering Destination</title>
@@ -53,6 +60,10 @@
 		
 	</script>
 <div id="loading-area"></div>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKNSJHM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header class="site-header header-style-4 dark">
 		<!-- top bar -->
 		<div class="top-bar">
