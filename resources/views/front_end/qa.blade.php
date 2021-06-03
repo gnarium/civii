6cd95ff4d7ae9f110698ed2e7beaf2dcfe7ef5a0
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-03 16:43:46 +0530

    add favicon

--- resources/views/front_end/qa.blade.php
+++ resources/views/front_end/qa.blade.php
@@ -12,9 +12,7 @@
 	<meta name="google-site-verification" content="ucTRTXLEo1IKrfkR8YoZtee9XpyNdwghNzkuH1hpZcU" />
 	<script src="{{ asset('user/front_end/js/combining.js')}}"></script>
 	<link rel="canonical" href="https://www.engineeringdestination.com"/>
-	<link rel="icon" href="{{ asset('user/front_end/images/fav.ico')}}" type="image/x-icon" />
-	
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+	<link rel="icon" href="{{ asset('user/front_end/images/Engineering Destination Favicon .webp')}}" type="image/x-icon" />
 	
 	<title>Engineering Destination</title>
 	
