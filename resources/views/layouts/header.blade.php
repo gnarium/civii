e0eb1fd9181dc8b4a11607927abbc68ab10e7f08
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-08 13:38:28 +0530

    header done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
 	<meta name="format-detection" content="telephone=no">
 	<meta name="google-site-verification" content="ucTRTXLEo1IKrfkR8YoZtee9XpyNdwghNzkuH1hpZcU" />
     <script data-ad-client="ca-pub-3775391448345901" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<script src="{{ asset('user/front_end/js/combining.js')}}"></script>
+	
 	<link rel="canonical" href="https://www.engineeringdestination.com"/>
 	<link rel="icon" href="{{ asset('user/front_end/images/Engineering Destination Favicon .webp')}}" type="image/x-icon"  style="height: 300px;"/>
 	
@@ -60,3 +60,118 @@
 		
 	</script>
 </head>
+<body>
+	<header class="site-header header-style-4 dark">
+		<!-- top bar -->
+		<div class="top-bar">
+			<div class="container">
+				<div class="row d-flex justify-content-between">
+					<div class="dez-topbar-left">
+						<ul class="social-line text-center pull-right">
+							<li><a href="javascript:void(0);"> <span> AN ISO 9001:2015 Certified Youtube Channel</span> </a></li>
+						</ul>
+					</div>                        
+					<div class="dez-topbar-right">
+						<ul class="social-line text-center pull-right">
+							<li><a href="https://www.facebook.com/CIVIL-ENGG-Destination-100649208640067" class="fa fa-facebook"></a></li>
+							<li><a href="https://t.me/civilenggdestination" class="fa fa-telegram"></a></li>
+							<li><a href="https://www.linkedin.com/in/civilengineeringdestination-43255b1b6/" class="fa fa-linkedin"></a></li>
+							<li><a href="https://www.instagram.com/civilengineeringdestination" class="fa fa-instagram"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- top bar END-->
+		<div class="main-bar">
+			<div class="container header-contant-block">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="dez-logo "><a href="/"><img src="{{ asset('user/front_end/images/logooo.webp')}}" width="193" height="89" alt=""></a></div>
+					</div>
+					<div class="col-md-8">
+  						 
+					<ul class="contact-info clearfix" >
+  						
+					<li style="margin-left:100px;">
+						
+					<a class="site-button m-r15 text-white text-center btn-block" href="https://www.youtube.com/channel/UCzmD7qEkkg4ltvAp-bJhSWg?sub_confirmation=1" style="margin-top:-10px;">  
+									<p class="m-a0">Follow Us On</p>   
+					<p class="m-a0">Career Destination</p>
+									<!-- <p class="m-a0">+91 8899296385</p>   -->
+								</a>
+								
+							</li>
+							<li>
+							<a class="site-button text-white text-center " href="https://www.youtube.com/channel/UCQRvGmqysawXYkbs0VWoBeQ?sub_confirmation=1"style="margin-left:10px;margin-top:-10px;" >  
+							<p class="m-a0">Follow Us On</p>		
+							<p class="m-a0">Tech Civil Destination</p>
+								</a>
+							</li>
+							<li>
+							<a class="site-button text-white text-center " href="https://www.youtube.com/channel/UCngfRnQGedv6a3uB0APK6Mw?sub_confirmation=1"style="margin-left:10px;margin-top:0px;" >  
+							<p class="m-a0">Follow Us On</p>		
+							<p class="m-a0">Civil Engineering Destination</p>
+								</a>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>		<!-- main header -->
+		<div class="sticky-header main-bar-wraper navbar-expand-lg">
+			<div class="main-bar clearfix ">
+				<div class="slide-up">
+					<div class="container clearfix bg-primary">
+					
+						<!-- website logo --><div class="dez-logo mostion"><a href="/"><img src="{{ asset('user/front_end/images/logooo.png')}}" width="193" height="89" alt=""></a></div>
+							
+						<!-- nav toggle button -->
+						<button class="navbar-toggler collapsed navicon justify-content-end" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+							<span></span>
+							<span></span>
+							<span></span>
+						</button>
+						
+						<!-- extra nav -->
+						<!-- main nav -->
+						<div class="header-nav navbar-collapse collapse justify-content-start" id="navbarNavDropdown">
+							<ul class=" nav navbar-nav">
+							<li class="active"> <a href="/">Home</a>
+							</li>
+							<li> <a href="/about">About</a>
+								
+							</li>
+							<li> <a href="/govtjob">Government Job's</a>
+									</li>
+							<li> <a href="/privatejob">Private Job's</a>
+								
+							</li>
+							<li> <a href="/iscode">IS CODE</a>
+								
+							</li>
+
+							<li> <a href="/qa">QA/QC TESTING</a>
+								
+							</li>
+							
+							<li> <a href="/privacy">Privacy</a>
+								
+							</li>
+
+							<li> <a href="/disclaimer">Disclaimer</a>
+								
+							</li>
+							<li> <a href="/contact">Contact us </a>
+								
+							</li>
+						</ul>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+		<!-- main header END -->
+	</header>
+@yield('content')
