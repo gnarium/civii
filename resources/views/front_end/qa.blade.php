2ed032895fc4e9876b2bc444edc23463f8f59f20
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-14 00:05:44 +0530

    ads add

--- resources/views/front_end/qa.blade.php
+++ resources/views/front_end/qa.blade.php
@@ -31,12 +31,33 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     <div class="col-lg-12 col-md-7">
                         <h1 class="m-b20 m-t0">QA / QC Testing</h1>
                         <div class="dez-separator bg-primary"></div>
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- QA QC 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="5524251583"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
                         <p>QA/QC stands for quality assurance quality control. For any construction project to be executed it needs to be approved from many government organizations and must carry some standards. All standards and parameters are set the related authority of the project and by government of India.</p>
 						
                         <p>QA/QC testing is the combination of quality assurance quality control tests performed in QA/QC labs of various construction projects to ensure proper safety and standards for any building or project constructed the test includes the process or set of processes and tests done by various instruments used to find the quality of the materials used for building the very same project. After all the test and reports are sanctioned by the respective authorities, the project is approved and launched for public use.</p>
 						<p>All projects like infrastructure, bridges, tunnels, roads have some common and some different types of testing to check their quality standards. Engineering students can also view them for better understanding and knowledge of civil engineering concepts and gaining new skills.</p>
 						<p>Preparing reports and data sheets is one of the most important work in any quality works. All report is prepared by civil engineers in QA/QC category. However, engineers find very difficulty in finding the right format for QA/QC test reports. Engineering destination provides all test reports formats for easy use and making of test reports. All formats are in excel formats and as per all revised IS codes issued by government of India.  All test formats have all details with all specifications needed for any project. All formats are free from any water marks or any symbol so the user will not face any problem or trouble in editing or making changes as per their requirement.If you need any other test format contact us on our email address or in our contact us page. We will try to provide all required test formats as soon as possible.</p>						
         				<b>Note:</b><p> all reports are prepared by team engineering destination and are not copied from any other website.</p>
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="5332679892"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 						</div>
                     <!-- Left part start END -->
                 </div>
@@ -50,24 +71,37 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     
 	<div class="container">
         <div class="row">
-            <div class="col-lg-12">
-      <table class="table container">
-        <thead>
-          <tr>
-            <th data-breakpoints="xs">ID</th>
-            <th>QA/ QC Testing Number</th>
-           </tr>
-        </thead>
-        @foreach($todoArr as $todo)
-            <tbody>
-                <tr>
-                    <th data-breakpoints="xs">{{$todo->id}}</th>
-                    <th><a href="{{$todo->url}}">{{$todo->qaname}}</a></th>
-                </tr>
-       </tbody>
-        @endforeach
-    </table>
-    </div>
+            <div class="col-lg-9">
+                <table class="table container">
+                    <thead>
+                        <tr>
+                            <th data-breakpoints="xs">ID</th>
+                               <th>QA/ QC Testing Number</th>
+                         </tr>
+                    </thead>
+                    @foreach($todoArr as $todo)
+                        <tbody>
+                            <tr>
+                                <th data-breakpoints="xs">{{$todo->id}}</th>
+                                <th><a href="{{$todo->url}}">{{$todo->qaname}}</a></th>
+                            </tr>
+                   </tbody>
+                    @endforeach
+                </table>
+            </div>
+            <div class="col-lg-3>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- QA QC 4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="2537967925"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+            </div>
     </div>
 </div>   
 
@@ -76,4 +110,15 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 </section>
 </section>
     </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- QA QC 3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="6691167054"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     @endsection
