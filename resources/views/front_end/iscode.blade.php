6071bf0fbb5745936a4f535d88d323f50207e427
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-14 00:01:58 +0530

    ads add

--- resources/views/front_end/iscode.blade.php
+++ resources/views/front_end/iscode.blade.php
@@ -29,13 +29,34 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     <div class="col-lg-12 col-md-7" >
                         <h1 class="m-b20 m-t0">IS CODE</h1>
                         <div class="dez-separator bg-primary"></div>
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Is code 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="9655068286"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
                         <p style="text-align:justify;">Indian Standard Codes IS codes are pre-defined set of instructions and information provided by government of India for different types of construction, repair and maintenance works in India. IS codes are released in different years containing essential and important information for execution of any project. All construction works are done according to them. IS codes are set of information that are observed by engineers from there experience and knowledge. Some of the information are only practical knowledge and does not have any proof or theory. They are tentative and are revised over time to time. All is codes are downloaded for government websites with proper authority. We do not violate any rule or law. This Is for knowledge purpose only.</p>
 						
                         <p style="text-align:justify;">IS codes are also touched in engineering college for understanding and better knowledge of construction projects and understanding concepts of civil engineering by students.IS codes are revised in every few years. IS codes are updated with respect to time with new methods and details as per new construction methods.</p>
 						<p style="text-align:justify;">All projects like infrastructure, bridges, tunnels, roads have some common and some different types of testing to check their quality standards. Engineering students can also view them for better understanding and knowledge of civil engineering concepts and gaining new skills.</p>
 						<p style="text-align:justify;">However. It is very difficult to find all is codes on a single platform. In engineering destination, we provide all is codes for students and professionals. All versions of all IS codes and revised editions are uploaded in this page where you can read them, view them and even download option is also provided for easy convenience of the user. All IS codes are copy right free and can be used for any purpose. No fees or any monthly subscription is required to view or download any IS code.</p>						
         				<b>Note:</b><p style="text-align:justify;"> If you need any other IS code then please contact us on our e-mail address or on our contact page. We will try to provide all your needed is code as soon as possible. For any related query or any problem contact us on our contact us page on website.</p>
-						</div>
+						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="8341986619"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+                        </div>
                     <!-- Left part start END -->
                 </div>
             </div>
@@ -82,12 +103,33 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </table>
     </div>
     <div class="col-lg-3">
-    
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- IS Code 4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="7981866296"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     </div>
     </div>
     </div>
   </div>
 </div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- IS Code 3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="8138860532"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 </section>
 
     </div>
