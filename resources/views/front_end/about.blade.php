a3e7e1f5241ac6eb7018e2bd65b9ec28da416a0a
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-13 23:53:30 +0530

    end

--- resources/views/front_end/about.blade.php
+++ resources/views/front_end/about.blade.php
@@ -33,12 +33,34 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						<div class="col-lg-5 m-b30 hidden-sm">
                             <div class="dez-thu "><img src="{{ asset('user/front_end/images/A D SIR.png')}}" alt=""></div>
                         </div>
+                        
                         <div class="col-lg-7">
-                            <h2 class="h2 m-t0"> About Engineering <span class="text-primary">Destination </span></h2>
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- About 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="1257703511"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+                            <h2 class="h2 m-t0"> About  <span class="text-primary">Engineering Destination </span></h2>
                             <div class="dez-separator bg-primary"></div>
                             <div class="clear"></div>
                             <p class="m-b30"style="text-align:justify;">Engineering Destination is the official website of Civil Engineering Destination an ISO 9001:2015 certified channel owned by Anmol Dainesh Singh (A. D Singh) graduated B. Tech in civil engineering from central government university. Civil Engineering Destination was founded on 4 September 2020 After years of service and experience in big construction companies and government organizations and big projects like infrastructure, bridges, tunnels and roads.  I have decided to work for the improvement and career counselling of engineering students and graduates.</p><p class="m-b30"style="text-align:justify;">Engineering destination provides all jobs details and links. Other helpful information like IS codes and QA/QC test report formats are also available on our website. We also provide current affairs related to engineering and technical classes for all competitive exams in India.</p>
                             <p class="m-b30" style="text-align:justify;">Civil Engineering destination is an ISO 9001:2015 certified channel.
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="7631540171"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 							<p class="m-b30" style="text-align:justify;">Engineering destination is the most trusted platform for all engineering students and professionals. We have members of all age groups as well as from different fields of engineering looking for better jobs and career counselling. Civil engineering destination provides most authentic and latest job updates for all engineering branches.We also aim at counselling and motivating students and graduates for getting the best job as per their requirements by special video sessions, special courses and internship programs organized with top institutions and organizations of India.Career Destination by A.D Singh and tech civil destination is also a part of civil engineering destination where we help students those who are preparing for competitive government exams.Career Destination by A.D Singh is a secondary channel and a wing of civil engineering destination where we provide all seminars and knowledge of civil engineering certificate programs and training sessions conducted by various organizations of India and various other helpful information is also provided.</p>
 							<p class="m-b30" style="text-align:justify;">A.D Singh has specializations in many engineering fields.In the world of construction an engineer has to master some of the main construction related works are as under</p>
 							<div class="row">
@@ -97,6 +119,17 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                     </div>
                                 </div>
                             </div>
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- About 3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="9682988445"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
                         </div>
                     </div>
                 </div>
