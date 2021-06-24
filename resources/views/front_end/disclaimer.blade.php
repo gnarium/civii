9066edc182adc1cf29f989c2125993003e46a022
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-25 00:28:25 +0530

    git commit

--- resources/views/front_end/disclaimer.blade.php
+++ resources/views/front_end/disclaimer.blade.php
@@ -32,23 +32,22 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <div class="container">
                 <div class="row">
                     <!-- Left part start -->
-                    <div class="col-lg-12 col-md-7">
+                    <div class="col-lg-8 col-md-7">
                         <h1 class="m-b20 m-t0">DISCLAIMER</h1>
                         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Disclaimer 1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="3061739778"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                        <!-- Disclaimer 1 -->
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-3775391448345901"
+                             data-ad-slot="3061739778"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>
+                        <script>
+                             (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                         <div class="dez-separator bg-primary"></div>
                         <p>The information provided by Engineering Destination ("we, us" or "our") on https://engineeringdestination.com (the "Site") is for general informational purposes only. All information on the Site is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity. reliability, availability or completeness of any information on the Site Under no circumstance shall we have any liability to you for any loss or damage of any kind incurred as a result of the use of the site or reliance on any information provided on the site. Your use of the site and your reliance on any information on the site is solely at your own risk. </p>
-							<h1 class="m-b20 m-t0">EXTERNAL LINKS DISCLAIMER
-</h1>
+							<h1 class="m-b20 m-t0">EXTERNAL LINKS DISCLAIMER</h1>
                         <div class="dez-separator bg-primary"></div>
                         <p>The Site may contain (or you may be sent through the Site) links to other websites or content belonging to or originating from third parties or links to websites and features in banners or other advertising Such external links are not investigated, monitored, or checked for accuracy adequacy validity, reliability availability or completeness by us.We do not warrant, endorse, guarantee, or assume responsibility for the accuracy or reliability of any information offered by third-party websites linked through the site or any website or feature linked in any banner or other advertising we will not be a party to or in any way be responsible for monitoring any transaction between you and third-party providers of products or services.</p>
 						<h1 class="m-b20 m-t0">PROFESSIONAL DISCLAIMER</h1>
@@ -56,35 +55,60 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                         
                         <p>The Site cannot and does not contain job news updates advice. The job news updates information is provided for general informational and educational purposes only and is not a substitute for professional advice. Accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. We do not provide any kind of job news updates advice. The use or reliance of any information contained on this site is solely at your own risk.</p>
 						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Disclaimer 3 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="5496331427"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                        <!-- Disclaimer 3 -->
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-3775391448345901"
+                             data-ad-slot="5496331427"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>
+                        <script>
+                             (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                         <h1 class="m-b20 m-t0">TESTIMONIALS DISCLAIMER</h1>
                         <div class="dez-separator bg-primary"></div>
                         <p>The Site may contain testimonials by users of our products and/or services. These testimonials reflect the real-life experiences and opinions of such users. However the experiences are personal to those particular users and may not necessarily be representative of all users of our products and/or services We do not claim, and you should not assume that all users will have the same experiences. Your individual results may vary.</p>
                             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Disclaimer 2 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="1560175596"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                        <!-- Disclaimer 2 -->
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-3775391448345901"
+                             data-ad-slot="1560175596"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>
+                        <script>
+                             (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
         				</div>
                         
                     <!-- Left part start END -->
                     <!-- Right part start -->
                                     </div>
+            </div>
+            <div class="col-lg-3">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Disclaimer 4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="4276804437"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Disclaimer 4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="4276804437"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
             </div>
             <!-- Career END -->
         </div>
