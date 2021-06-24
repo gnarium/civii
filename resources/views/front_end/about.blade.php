88dada72cd0b99df98f1de3a5d950088983e7844
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-25 00:18:11 +0530

    Comit changes vertical about

--- resources/views/front_end/about.blade.php
+++ resources/views/front_end/about.blade.php
@@ -136,6 +136,18 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- About 5 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="7448180104"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 
                         </div>
                      
