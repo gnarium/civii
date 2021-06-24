57af1e76afe40b777f7d4fcd2f952d7685e2beda
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-25 00:40:42 +0530

    done

--- resources/views/front_end/privacy.blade.php
+++ resources/views/front_end/privacy.blade.php
@@ -113,18 +113,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						<h2 class="m-b20 m-t0">Children's Information</h2>
 						<p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.Engineering Destination does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
 						</ul>
-                         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                        <!-- Privacy 3 -->
-                        <ins class="adsbygoogle"
-                         style="display:block"
-                         data-ad-client="ca-pub-3775391448345901"
-                         data-ad-slot="6094069556"
-                         data-ad-format="auto"
-                         data-full-width-responsive="true"></ins>
-                        <script>
-                         (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>
-                        <div class="col-lg-4>
+                         <div class="col-lg-4>
                         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Privacy 4 -->
 <ins class="adsbygoogle"
@@ -182,7 +171,19 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 </script>
 
                         </div>
-						</div> 
+						
+                         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- Privacy 3 -->
+                        <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-3775391448345901"
+                         data-ad-slot="6094069556"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                        <script>
+                         (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                       </div> 
                     <!-- Left part start END -->
                     <!-- Right part start -->
                                         <!-- Right part start END -->
