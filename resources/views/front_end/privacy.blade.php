8e54778a73d3d738242bf1f1422805688da5d672
gnarium <80353633+gnarium@users.noreply.github.com>
2021-06-27 20:08:59 +0530

    privacy dome

--- resources/views/front_end/privacy.blade.php
+++ resources/views/front_end/privacy.blade.php
@@ -135,6 +135,34 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                             data-full-width-responsive="true"></ins><script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                             </script>
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- Privacy 4 -->
+                            <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-3775391448345901"
+                            data-ad-slot="5662872448"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins><script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- Privacy 4 -->
+                            <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-3775391448345901"
+                            data-ad-slot="5662872448"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins><script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- Privacy 4 -->
+                            <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-3775391448345901"
+                            data-ad-slot="5662872448"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins><script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
                        </div>
                         
                 
