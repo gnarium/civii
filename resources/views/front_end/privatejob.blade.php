110d04d7a10a91e7fdf2c1b2f50a62b907b98c9c
gnarium <80353633+gnarium@users.noreply.github.com>
2021-07-04 22:40:04 +0530

    done

--- resources/views/front_end/privatejob.blade.php
+++ resources/views/front_end/privatejob.blade.php
@@ -26,6 +26,18 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         </div>
     </div>
         <div class="section-full bg-white content-inner">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- private 1 -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-3775391448345901"
+                     data-ad-slot="5930418023"
+                     data-ad-format="auto"
+                     data-full-width-responsive="true"></ins>
+                <script>
+                     (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+
             <!-- Product -->
             <div class="container">
 				<div class="row">
