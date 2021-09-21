ad14f75fe3523e228c1f9576579a31ce8488a548
gnarium <80353633+gnarium@users.noreply.github.com>
2021-09-21 23:06:08 +0530

    done

--- resources/views/front_end/govtjob.blade.php
+++ resources/views/front_end/govtjob.blade.php
@@ -24,7 +24,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div class="breadcrumb-row">
             <div class="container">
                 <ul class="list-inline">
-                    <li><a href="/">Home</a></li>
+                    <li><a href="/" style="color:black;">Home</a></li>
                     <li>Government Job</li>
                 </ul>
             </div>
