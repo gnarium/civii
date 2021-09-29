b45aea194b2e1a31713049b9ebca64240edabceb
gnarium <80353633+gnarium@users.noreply.github.com>
2021-09-29 10:14:37 +0530

    Add files via upload
    add file

---
+++ resources/views/front_end/terms-and-conditions.blade.php
@@ -0,0 +1,73 @@
+<meta property="og:title" content="Term And Condition"/>
+<meta name="description" content="Engineer Destination has all rights reserved to change and modify its data without consent from anyone.">
+ 
+@extends('layouts.main')
+@section('content')
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKNSJHM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<div class="page-content bg-white">
+        <!-- inner page banner -->
+        <div class="dez-bnr-inr overlay-black-middle" style="background-image:url('{{ asset('user/front_end/images/background/bg4.jpg')}}');">
+            <div class="container">
+                <div class="dez-bnr-inr-entry">
+                    <h1 class="text-white">Privacy Policy</h1>
+                </div>
+            </div>
+        </div>
+        <!-- inner page banner END -->
+        <!-- Breadcrumb row -->
+        <div class="breadcrumb-row">
+            <div class="container">
+                <ul class="list-inline">
+                    <li><a href="/" style="color:black;">Home</a></li>
+                    <li>Term And Conditions</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Breadcrumb row END -->
+        <!-- contact area -->
+        <div class="content-area">
+            <!-- Career -->
+            <div class="container">
+                <div class="row">
+                    <!-- Left part start -->
+                    <div class="col-lg-8 col-md-7">
+                        <h1 class="m-b20 m-t0">Terms and Conditions :</h1>
+                        <p style="text-align:justify;">The terms and condition of our platform or website "Engineering Destination" are given below :</p>
+						<div class="dez-separator bg-primary"></div>
+						 <ul class="list-check-circle primary">
+						    <li>Engineering Destination has all rights to modify and delete  any service or product provided on our platform without providing any prior consent and information to any user or buyer . </li>
+                            <li>Every product or service provided  at our platform is the sole property of "Engineering Destination" </li>
+                            <li>"Engineering Destination " contains all rights to change data or modify it from our platform without informing anyone .</li>
+                        </ul>
+                        <h3>Refund Policy :</h3>
+                        <p>The refund policy for any service or product on Engineering Destination website or on our YouTube channel</p> 
+                        <ul class="list-check-circle primary">
+                            <li>https://youtube.com/channel/UCngfRnQGedv6a3uB0APK6Mw</li>
+                            <li> The registration fees of any 
+                            The registration fees of any 
+                                ●	Courses which is or will be conducted on our platform once paid will be non refundable .
+                                ●	Webinar which will be conducted on our platform once paid will be non refundable 
+                            </li>
+                            <li>Product or service fees will also be non refundable.</li>
+                                                </ul>
+                       <p style="text-align:justify;">Users can find more specific service and product terms and conditions on the payment pages created using payment gateway platforms i.e "Razorpay" etc.  Or on the product or service pages created on our website toor on our YouTube channel .</p>
+						 </div>
+                        
+                
+                        
+                    
+                
+                        </div>
+                        
+                        
+            </div>
+           </div>
+            <!-- Career END -->
+        </div>
+        <!-- contact area END -->
+    
+    @endsection
