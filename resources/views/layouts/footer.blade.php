4ae2206e8e8add0128788ac2432dc1804cb4df6e
gnarium <80353633+gnarium@users.noreply.github.com>
2021-10-01 08:43:40 +0530

    done

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,12 +15,12 @@
 							<div class="dez-separator bg-primary"></div>
 							<ul>
 								<li><a href="/about">About</a></li>
-								<li><a href="/govtjob">Government Jobs</a></li>
-								<li><a href="/privatejob">Private Jobs</a></li>
-								<li><a href="/privacy">Privacy & Policy</a></li>
-								<li><a href="/contact">Contact</a></li>
 								<li><a href="/iscode">IS Codes</a></li>
 								<li><a href="/qa">QA/QC Reports</a></li>
+                                <li><a href="/contact">Contact</a></li>
+								<li><a href="/privacy">Privacy & Policy</a></li>
+								<li><a href="/privatejob">Disclaimer</a></li>
+                                <li><a href="/privatejob">Terms-And-Conditions</a></li>
 							</ul>
 						</div>
 					</div>
