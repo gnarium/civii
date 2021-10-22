38d476c2cb697c35b3847d3c4bb2ce3bd8238675
gnarium <80353633+gnarium@users.noreply.github.com>
2021-10-22 13:09:44 +0530

    gov job disc 2 updated

--- resources/views/front_end/jobdescribtion.blade.php
+++ resources/views/front_end/jobdescribtion.blade.php
@@ -48,17 +48,18 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                                  (adsbygoogle = window.adsbygoogle || []).push({});
                             </script>
                                                     <p><strong>{!!$todoArr14->describtion!!}</strong></p>
-                                                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                            <!-- Job Disc 2 -->
-                            <ins class="adsbygoogle"
-                                 style="display:block"
-                                 data-ad-client="ca-pub-3775391448345901"
-                                 data-ad-slot="5704268889"
-                                 data-ad-format="auto"
-                                 data-full-width-responsive="true"></ins>
-                            <script>
-                                 (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
+                                                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
+     crossorigin="anonymous"></script>
+<!-- GOV Job Disc 2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3775391448345901"
+     data-ad-slot="5704268889"
+     data-ad-format="auto"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
                      </div>
                      <div class="col-lg-3">
                               <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
