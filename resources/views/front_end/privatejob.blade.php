f69fe189726f030b2a65f8c4b28c5546d6623a5b
gnarium <80353633+gnarium@users.noreply.github.com>
2021-10-28 19:33:55 +0530

    remove ads

--- resources/views/front_end/privatejob.blade.php
+++ resources/views/front_end/privatejob.blade.php
@@ -34,19 +34,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                            
             <!-- Product -->
             <div class="container">
-             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
-     crossorigin="anonymous"></script>
-<!-- private 1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="5930418023"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
+             
 				<div class="row">
 					<div class="col-lg-10 col-md-7 col-sm-12">
 						<div class="text-center m-b50">
@@ -84,46 +72,12 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                         </div>
 						</div>
                         <div class="col-lg-2">
-                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
-     crossorigin="anonymous"></script>
-<!-- Private 3 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="9091777725"
-     data-ad-format="auto"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
-     crossorigin="anonymous"></script>
-<!-- Private 3 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="9091777725"
-     data-ad-format="auto"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
+                                                    
                                 
                         </div>
                         
                 </div>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3775391448345901"
-     crossorigin="anonymous"></script>
-<!-- Private 2 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3775391448345901"
-     data-ad-slot="1991173013"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                
 
 		   </div>
         </div>
